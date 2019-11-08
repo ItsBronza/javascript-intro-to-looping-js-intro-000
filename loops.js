8b404f444b177ce18f96fdaad1f6b1effdcec310
Alyona <itsbronza@yahoo.com>
2019-11-08 07:02:16 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 function forLoop(array){
 
   for(i = 0; i < 25; i++){
-    return `"I am ${i} strange loop${i === 0 ? '': 's'}."`
+    return `"I am ${i} strange loop${i === 0 ? '' : 's'}."`
   }
 
 }
